4c188f40133f5d2f659af16afd9568b07ac882bc
Abinav <Abinavbukkaraya@gmail.com>
2018-01-09 22:46:41 -0500

    Shifted everything to main page

--- Bukkaraya/urls.py
+++ Bukkaraya/urls.py
@@ -18,12 +18,9 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import include
-from .views import home, contact, projects
+from .views import home
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contact/', contact, name='contact'),
-    path('projects/', projects, name='projects'),
-    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
